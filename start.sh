fa609c3e0b1be20d79c1d9e87e55152392fe4da3
Mayavi333 <105354161+Mayavi333@users.noreply.github.com>
2022-07-10 17:30:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Mayavi333/LUTTAPPI-BOTS.git/PROFESSOR-BOT
+  git clone https://github.com/Mayavi333/LUTTAPPI-BOTSzz.git/PROFESSOR-BOT
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /PROFESSOR-BOT
